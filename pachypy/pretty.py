906f40665d11df9654563ff49b3ec9fd61ef6af3
Simon Gurcke <simon.gurcke@skyfii.com>
2019-03-23 02:54:34 +1100

    Style repos and better ordering for pipelines

--- pachypy/pretty.py
+++ pachypy/pretty.py
@@ -2,6 +2,7 @@ __all__ = [
     'PrettyPachydermClient'
 ]
 
+from typing import Dict, List
 from datetime import datetime
 
 import pandas.io.formats.style as style
@@ -12,31 +13,35 @@ from .client import PachydermClient
 
 
 STYLE_HIGHLIGHT_CSS = 'color: #d65f5f; font-weight: bold'
-STYLE_BAR_COLOR = '#d65f5f44'
+STYLE_BAR_COLOR = '#105ecd33'
 
 
 class PrettyPachydermClient(PachydermClient):
 
     def list_repos(self, repos: str = '*') -> style.Styler:
-        df = super().list_repos(repos=repos)
-        return df.style \
-            .apply(_style_highlight_gt1, subset=['branches']) \
-            .apply(_style_highlight_true, subset=['is_tick']) \
-            .bar(subset=['size_megabytes'], color=STYLE_BAR_COLOR)
+        df = super().list_repos(repos=repos).reset_index()
+        df.rename({
+            'repo': 'Repo',
+            'is_tick': 'Tick',
+            'branches': 'Branches',
+            'size_bytes': 'Size',
+            'created': 'Created',
+        }, axis=1, inplace=True)
+        df['Tick'] = df['Tick'].map({True: emojize(':stopwatch:'), False: ''})
+        df['Branches'] = df['Branches'].apply(', '.join)
+        return df[['Repo', 'Tick', 'Branches', 'Size', 'Created']].style \
+            .bar(subset=['Size'], color=STYLE_BAR_COLOR) \
+            .format({'Created': _format_date, 'Size': _format_size}) \
+            .set_properties(**{'text-align': 'left'}) \
+            .set_properties(subset=['Size', 'Created'], **{'white-space': 'nowrap'}) \
+            .set_table_styles([dict(selector='th', props=[('text-align', 'left'), ('white-space', 'nowrap')])]) \
+            .hide_index()
 
     def list_pipelines(self, pipelines: str = '*') -> style.Styler:
-        df = super().list_pipelines(pipelines=pipelines).reset_index()
-        df['Parallelism'] = ''
-        df.loc[df['parallelism_constant'] > 0, 'Parallelism'] = \
-            emojize(':keycap_#: ', use_aliases=True) + df['parallelism_constant'].astype(str)
-        df.loc[df['parallelism_coefficient'] > 0, 'Parallelism'] = \
-            emojize(':keycap_*: ', use_aliases=True) + df['parallelism_coefficient'].astype(str)
-        df['Jobs S/F'] = \
-            '<span style="' + df['jobs_success'].apply(lambda x: 'color: green' if x > 0 else '') + '">' + \
-            df['jobs_success'].astype(str) + \
-            '</span> / <span style="' + df['jobs_failure'].apply(lambda x: 'color: red' if x > 0 else '') + '">' + \
-            df['jobs_failure'].astype(str) + \
-            '</span>'
+        df = super().list_pipelines(pipelines=pipelines)
+        df['sort_key'] = df.index.map(_calc_pipeline_sort_key(df['input_repos'].to_dict()))
+        df.reset_index(inplace=True)
+        df.sort_values('sort_key', inplace=True)
         df.rename({
             'pipeline': 'Pipeline',
             'state': 'State',
@@ -56,11 +61,23 @@ class PrettyPachydermClient(PachydermClient):
         })
         df.loc[df['jobs_running'] > 0, 'State'] = emojize(':running: job running', use_aliases=True),
         df.loc[df['Cron'] != '', 'Cron'] = emojize(':stopwatch:') + ' ' + df['Cron']
-        return df[['Pipeline', 'State', 'Cron', 'Input', 'Output', 'Tries', 'Parallelism', 'Jobs S/F', 'Created']].style \
-            .format({'Created': _format_date_natural}) \
+        df['Parallelism'] = ''
+        df.loc[df['parallelism_constant'] > 0, 'Parallelism'] = \
+            emojize(':keycap_#: ', use_aliases=True) + df['parallelism_constant'].astype(str)
+        df.loc[df['parallelism_coefficient'] > 0, 'Parallelism'] = \
+            emojize(':keycap_*: ', use_aliases=True) + df['parallelism_coefficient'].astype(str)
+        df['Jobs'] = \
+            '<span style="' + df['jobs_success'].apply(lambda x: 'color: green' if x > 0 else '') + '">' + \
+            df['jobs_success'].astype(str) + \
+            '</span> / ' + \
+            '<span style="' + df['jobs_failure'].apply(lambda x: 'color: red' if x > 0 else '') + '">' + \
+            df['jobs_failure'].astype(str) + \
+            '</span>'
+        return df[['Pipeline', 'State', 'Cron', 'Input', 'Output', 'Tries', 'Parallelism', 'Jobs', 'Created']].style \
             .apply(_style_state, subset=['State']) \
+            .format({'Created': _format_date}) \
             .set_properties(**{'text-align': 'left'}) \
-            .set_properties(subset=['State', 'Cron', 'Jobs S/F'], **{'white-space': 'nowrap'}) \
+            .set_properties(subset=['State', 'Cron', 'Jobs', 'Created'], **{'white-space': 'nowrap'}) \
             .set_table_styles([dict(selector='th', props=[('text-align', 'left')])]) \
             .hide_index()
 
@@ -93,10 +110,46 @@ class PrettyPachydermClient(PachydermClient):
             worker = row.worker
 
 
-def _format_date_natural(d):
+def _calc_pipeline_sort_key(input_repos: Dict[str, List[str]]):
+    def get_dag_distance(p, i=0):
+        yield i
+        for d in input_repos[p]:
+            if d in pipelines:
+                yield from get_dag_distance(d, i + 1)
+
+    def get_dag_dependencies(p):
+        yield p
+        for d in input_repos[p]:
+            if d in pipelines:
+                yield from get_dag_dependencies(d)
+
+    pipelines = set(input_repos.keys())
+    dag_distance = {p: max(list(get_dag_distance(p))) for p in pipelines}
+    dag_nodes = {p: set(get_dag_dependencies(p)) for p in pipelines}
+    for p, nodes in dag_nodes.items():
+        for node in nodes:
+            dag_nodes[node].update(nodes)
+    dag_name = {p: min(nodes) for p, nodes in dag_nodes.items()}
+    return {p: f'{dag_name[p]}/{dag_distance[p]}' for p in pipelines}
+
+
+def _format_date(d):
     td = (datetime.now().date() - d.date()).days
     word = {-1: 'Tomorrow', 0: 'Today', 1: 'Yesterday'}
     return (word[td] if td in word else '{:%-d %b %Y}'.format(d)) + ' at {:%H:%M}'.format(d)
+
+
+def _format_size(num):
+    if abs(num) == 1:
+        return f'{num:.0f} byte'
+    if abs(num) < 1000.0:
+        return f'{num:.0f} bytes'
+    num /= 1000.0
+    for unit in ['KB', 'MB', 'GB', 'TB']:
+        if abs(num) < 1000.0:
+            return f'{num:.1f} {unit}'
+        num /= 1000.0
+    return f'{num:,.1f} PB'
 
 
 def _style_highlight_true(s):
