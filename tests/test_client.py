8cbca4636f57488f91e7183955ff13a46eeb618a
Simon Gurcke <simon.gurcke@skyfii.com>
2019-03-29 15:30:06 +1100

    Add tests for list_commits(), list_files() and list_datums()

--- tests/test_client.py
+++ tests/test_client.py
@@ -13,9 +13,24 @@ def get_mock_from_csv(file, datetime_cols=None, timedelta_cols=None, json_cols=N
     return df
 
 
+def mock_list_commits(_, repo, n=20):
+    df = get_mock_from_csv('list_commits.csv', datetime_cols=['started', 'finished'])
+    return df[df['repo'] == repo].head(n)
+
+
+def mock_list_files(_, repo, commit=None, glob='**'):
+    df = get_mock_from_csv('list_files.csv', datetime_cols=['committed'])
+    return df[df['repo'] == repo]
+
+
 def mock_list_jobs(_, pipeline=None, n=20):
     df = get_mock_from_csv('list_jobs.csv', datetime_cols=['started', 'finished'], timedelta_cols=['download_time', 'process_time', 'upload_time'])
     return (df[df['pipeline'] == pipeline] if pipeline is not None else df).head(n)
+
+
+def mock_list_datums(_, job):
+    df = get_mock_from_csv('list_datums.csv', datetime_cols=['committed'])
+    return df[df['job'] == job]
 
 
 def mock_get_logs(_, pipeline=None):
@@ -27,9 +42,13 @@ def patch_adapter():
     return patch.multiple(
         'pachypy.adapter.PachydermAdapter',
         list_repos=lambda _: get_mock_from_csv('list_repos.csv', datetime_cols=['created']),
+        list_repo_names=lambda _: get_mock_from_csv('list_repos.csv')['repo'].tolist(),
+        list_commits=mock_list_commits,
+        list_files=mock_list_files,
         list_pipelines=lambda _: get_mock_from_csv('list_pipelines.csv', datetime_cols=['created']),
         list_pipeline_names=lambda _: get_mock_from_csv('list_pipelines.csv')['pipeline'].tolist(),
         list_jobs=mock_list_jobs,
+        list_datums=mock_list_datums,
         get_logs=mock_get_logs,
         create_repo=DEFAULT,
         delete_repo=DEFAULT,
@@ -72,6 +91,34 @@ def test_list_repos(client, **mocks):
 
 
 @patch_adapter()
+def test_list_commits(client, **mocks):
+    from pachypy.client import PachydermClientException
+    del mocks
+    df = client.list_commits('test_x_pipeline_*')
+    assert len(df) == 10
+    assert df['size_bytes'].gt(0).all()
+    with pytest.raises(PachydermClientException):
+        assert client.list_commits('repo_that_doesnt_exist')
+
+
+@patch_adapter()
+def test_list_files(client, **mocks):
+    from pachypy.client import PachydermClientException
+    del mocks
+    df = client.list_files('test_x_pipeline_*', files_only=True)
+    assert len(df) == 7
+    assert df['type'].eq('file').all()
+    assert df['size_bytes'].gt(0).all()
+    df = client.list_files('test_x_pipeline_3', commit='ac6572d02c1749d699281c8f52f6860d', files_only=False)
+    assert len(df) == 10
+    assert df['type'].eq('dir').any()
+    with pytest.raises(PachydermClientException):
+        assert client.list_files('test_x_pipeline_*', commit='ac6572d02c1749d699281c8f52f6860d')
+    with pytest.raises(PachydermClientException):
+        assert client.list_files('repo_that_doesnt_exist')
+
+
+@patch_adapter()
 def test_list_pipelines(client, **mocks):
     del mocks
     assert len(client.list_pipelines()) == 5
@@ -81,6 +128,7 @@ def test_list_pipelines(client, **mocks):
 
 @patch_adapter()
 def test_list_jobs(client, **mocks):
+    from pachypy.client import PachydermClientException
     del mocks
     df = client.list_jobs()
     assert len(df) == 8
@@ -89,15 +137,31 @@ def test_list_jobs(client, **mocks):
     assert len(client.list_jobs(n=5)) == 5
     assert len(client.list_jobs('test_x_pipeline_5')) == 2
     assert len(client.list_jobs('test_x_pipeline_*', n=1)) == 1
+    with pytest.raises(PachydermClientException):
+        assert client.list_jobs('pipeline_that_doesnt_exist')
+
+
+@patch_adapter()
+def test_list_datums(client, **mocks):
+    del mocks
+    df = client.list_datums('e26ccf65131b4b3d9087cebc2f944279')
+    assert len(df) == 10
+    assert df.index.nunique() == 5
+    assert df['repo'].nunique() == 2
+    assert df['size_bytes'].gt(0).all()
+    assert len(client.list_datums('job_id_with_no_datums')) == 0
 
 
 @patch_adapter()
 def test_get_logs(client, **mocks):
+    from pachypy.client import PachydermClientException
     del mocks
     assert len(client.get_logs('test_x_pipeline_5')) == 10
     assert len(client.get_logs('test_x_pipeline_5', user_only=True)) == 7
     assert len(client.get_logs('test_x_pipeline_5', last_job_only=False)) == 20
     assert len(client.get_logs('test_x_pipeline_5', user_only=True, last_job_only=False)) == 14
+    with pytest.raises(PachydermClientException):
+        assert client.get_logs('pipeline_that_doesnt_exist')
 
 
 @patch_adapter()
@@ -106,14 +170,17 @@ def test_create_delete_repos(client, **mocks):
     list_repo_names = 'pachypy.adapter.PachydermAdapter.list_repo_names'
     repos = ['test_repo_1', 'test_repo_2']
     with patch(list_repo_names, MagicMock(return_value=[])):
+        assert client.create_repos(repos[0]) == repos[:1]
         assert client.create_repos(repos) == repos
         assert client.delete_repos('test_repo_*') == []
     with patch(list_repo_names, MagicMock(return_value=repos[1:])):
+        assert client.create_repos(repos[1]) == []
         assert client.create_repos(repos) == repos[:1]
         assert client.delete_repos('test_repo_*') == repos[1:]
     with patch(list_repo_names, MagicMock(return_value=repos)):
         assert client.create_repos(repos) == []
         assert client.delete_repos('test_repo_*') == repos
+        assert client.delete_repos(repos) == repos
 
 
 def test_pipeline_spec_files(client, pipeline_spec_files_path):
@@ -147,6 +214,7 @@ def test_create_update_delete_pipelines(client, **mocks):
     with patch(list_pipeline_names, MagicMock(return_value=[])):
         assert client.create_pipelines('test_a*') == (pipelines, [], [])
     with patch(list_pipeline_names, MagicMock(return_value=pipelines)):
+        assert client.create_pipelines('test_a*') == ([], [], [])
         assert client.update_pipelines('test_a*') == ([], pipelines, [])
         assert client.update_pipelines('test_a*', recreate=True) == (pipelines, [], pipelines[::-1])
         assert client.delete_pipelines('test_a*') == pipelines[::-1]
